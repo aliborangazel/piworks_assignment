82e6b5e3bbefb2b419a4665d7c15309071bd9910
aliborangazel <103659807+aliborangazel@users.noreply.github.com>
2022-04-14 18:12:59 +0300

    Update vaccine_p2.py

--- vaccine_p2.py
+++ vaccine_p2.py
@@ -12,7 +12,7 @@ with open(filename_toRead, 'r') as csv_to_read:
     data_count = len(lines)
     lines = np.array(lines)
 
-    # read country by country. Once a country is complete, find its median. Compare with the previous min median.
+    # read country by country. Once a country is complete, find its median. Compare with the previous medians.
     country = "smt"
 
     highest_medians = {
@@ -32,9 +32,9 @@ with open(filename_toRead, 'r') as csv_to_read:
                 med = np.median(vaccinations[countryStartingIndex:counter])
                 countryStartingIndex = counter
                 min_median_key = min(highest_medians, key = highest_medians.get)
-                if (med > highest_medians[min_median_key]): # if the new median is smaller than max of the dict 
-                    highest_medians[country] = med          # add the new minimum
-                    highest_medians.pop(min_median_key)     # remove the maximum among the three medians
+                if (med > highest_medians[min_median_key]): # if the new median is bigger than min of the dict 
+                    highest_medians[country] = med          # add the new median
+                    highest_medians.pop(min_median_key)     # remove the min among the three medians
             country = line[0]
 
         counter += 1
@@ -46,7 +46,7 @@ with open(filename_toRead, 'r') as csv_to_read:
     if (med > highest_medians[min_median_key]): # if the new median is bigger than min of the dict 
         highest_medians[country] = med          # add the new max
         highest_medians.pop(min_median_key)     # remove the min among the three medians
-    
+                                                # sort the dictionary in decreasing order
     highest_medians = {key:value for key,value in sorted(highest_medians.items(), key= lambda item:item[1], reverse=True)}
     print("\nTop 3 Countries with Highest Medians:")
     for key in highest_medians.keys():
